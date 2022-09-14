1abcf35ca71ffcd393aac2f5d028310e6618d238
alvigabriel <claudio.alves20112011@hotmail.com>
2022-09-14 10:51:05 -0300

    add routes, criando método para retornar usuarios da api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { application } = require('express')
 const express = require('express')
 
+const userRoute = require('./routes/userRoutes')
+
 const app = express()
 const port = 5000
 
---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const fs = require('fs')//lidar com arquivos file system
+const { join } = fs //lidar com pasta de arquivos
+
+const filePath = join(__dirname, 'users.json')//interação com bd, armazenar usuarios
+//junção do nome do diretorio da rota de usuários e o nome do arquivo dos usuários
+
+const getUsers = () => {//função pra pegar os usuarios
+    const data = fs.existsSync(filePath)//verificar se existe o arquivo
+        ? fs.readFileSync(filePath)//se existir, leia de maneira assíncrona
+        : []//se n existir, retornar obj vazio
+
+        try{
+            return JSON.parse(data)//fzr o parse com os dados
+        } catch (error) {
+            return []//se der erro, retornar vazio
+        }
+}
