754d2822bb6ad7c897bc5048b38c981c35aeb5be
alvigabriel <claudio.alves20112011@hotmail.com>
2022-09-14 10:17:48 -0300

    corrigindo erros

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { application } = require('express')
 const express = require('express')
 
-const add = express()
+const app = express()
 const port = 5000
 
-application.listen(port, () => console.log('API rodando na porta 5000'))
+app.get('/', (req, res) => res.send('Olá, mundo pelo express'))
+
+app.listen(port, () => console.log('API rodando na porta 5000'))
